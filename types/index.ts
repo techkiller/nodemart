b0ae239f75abeaca9ab7b2ac5f964289cc902900
techkiller <pankajjaangir@gmail.com>
2025-07-21 12:05:11 +0530

    fresh

--- types/index.ts
+++
@@ -1,8 +0,0 @@
-import { insertProductSchema } from "@/lib/validators";
-import z from "zod";
-
-export type Product = z.infer<typeof insertProductSchema> & {
-    id: string;
-    rating: number;
-    createdAt: Date;
-}
